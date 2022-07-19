eb3aff81bf5e03df8e689a22bba9bd8e61064ad7
Alessandro Resta <alessandro.resta@proton.me>
2022-07-19 20:00:23 +0200

    Solve Euler 2

---
+++ exercises/euler2_test.go
@@ -0,0 +1,31 @@
+package exercises
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEvenFibonacci(t *testing.T) {
+	testCases := []struct {
+		given    int
+		expected int
+	}{
+		{
+			given:    10,
+			expected: 10,
+		},
+		{
+			given:    4000000,
+			expected: 4613732,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("given: %d, expected: %d", tc.given, tc.expected), func(t *testing.T) {
+			observed := evenFibonacci(tc.given)
+			assert.Equal(t, tc.expected, observed)
+		})
+	}
+}
